e4fa482f3a978e00836c6eb4faac8592697c52d4
WilyBox <bdoohan123@hotmail.com>
2026-02-15 14:37:28 +0000

    GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,22 +76,36 @@ export default function RootLayout({
     <html lang="en" className={poppins.variable}>
       <head>
 
-      {/* Google tag (gtag.js) */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-XCH9G9NCQQ"
-        strategy="afterInteractive"
-      />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-XCH9G9NCQQ');
-        `}
-      </Script>
+        <Script
+          id="gtm-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MLZ3KHTP');
+            `,
+          }}
+        />
+
 
       </head>
       <body className={`${poppins.variable} ${geistMono.variable} antialiased bg-white`}>
+
+        <noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-MLZ3KHTP"
+    height="0"
+    width="0"
+    style={{ display: 'none', visibility: 'hidden' }}
+  />
+</noscript>
+
         <Navbar /> {/* Include the Navbar here */}
         {children}
         <Footer />
